ee8b1b518ab2525b6db8a09183fb35cfb2e52e5d
Favour Sunday <sundayfavour997@gmail.com>
2024-10-16 16:16:08 +0100

    new wagmi chain

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,9 +1,9 @@
 
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { arbitrum, base, mainnet, optimism, polygon } from 'wagmi/chains';
+import { sepolia } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
   appName: 'Veryfai',
   projectId: '4e85b5d66032b0920d953d249c599b2e',
-  chains: [mainnet, polygon, optimism, arbitrum, base],
+  chains: [sepolia],
 });
